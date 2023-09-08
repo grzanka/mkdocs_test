9b1f6b1d1b9aca6a4edc6af4f48daaa1ae348515
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-09-08 18:01:45 +0200

    more files

---
+++ src/first.py
@@ -0,0 +1,6 @@
+class FirstClass:
+    def __init__(self):
+        self.name = "FirstClass"
+
+    def print_name(self):
+        print(self.name)
---
+++ src/second.py
@@ -0,0 +1,7 @@
+class SecondClass:
+    def __init__(self):
+        print("SecondClass init")
+        self.name = "SecondClass"
+    
+    def print_name(self):
+        print(self.name)
